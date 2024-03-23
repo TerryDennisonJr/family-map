96b34b432d95f880971719078a2cdb3066d4c448
Terry Dennison <terrydennison@Terrys-MacBook-Pro.local>
2024-03-23 17:29:30 -0600

    update of csv file

--- Family Map/us_map.py
+++ Family Map/us_map.py
@@ -73,14 +73,17 @@ state_code = re.findall("[A-Z]{2}", str(df["State"]))
 # interate through State Code values and update count in "state_vals" dictionary
 vals=0
 for x in state_vals:
-    if x in state_code:
-        vals+=1
-        state_vals.update({x: vals})
-        
+    state_vals.update({x: state_code.count(x)})
+
 # put updated data in a new dictionary and dataframe
 data = {"State": state_vals.keys(), "Val":state_vals.values()}
 list_df = pd.DataFrame(data)
 print(list_df)
+
+for x in state_vals:
+    if x in state_code:
+        vals+=1
+        state_vals.update({x: vals})
 
 # plot the dataframe in a map
 fig = go.Figure(data=go.Choropleth(
